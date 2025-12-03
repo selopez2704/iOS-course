78580d86d88bc35eee7d5e490d96af8fea244b03
Sebastian Lopez <sebastianlopezmazo@Sebastians-MacBook-Air.local>
2025-12-02 21:34:28 -0500

    xcode project created

---
+++ iOS-course/iOS_courseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_courseApp.swift
+//  iOS-course
+//
+//  Created by Sebastian Lopez Mazo on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_courseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
