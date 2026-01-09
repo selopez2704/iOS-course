d73cff22f0b73e1c7479903159d803c2b43e486f
Sebastian Lopez Mazo <51925867+selopez2704@users.noreply.github.com>
2026-01-08 20:31:25 -0500

    New custom textfield added

--- iOS-course/components/TextFieldExample.swift
+++ iOS-course/components/TextFieldExample.swift
@@ -18,6 +18,41 @@ struct TextFieldExample: View {
     }
 }
 
+
+struct FormExample: View {
+    @State var Fname:String = ""
+    @State var Lname:String = ""
+    
+    var body: some View {
+        VStack (spacing:36) {
+            InfoField(label: "First Name", text: $Fname)
+            InfoField(label: "Last Name", text: $Lname)
+        }.padding()
+    }
+}
+
+struct InfoField: View {
+    let label:String
+    @Binding var text:String
+    @FocusState var isTyping:Bool
+    var body: some View {
+        ZStack(alignment: .leading) {
+            TextField("", text: $text).padding(.leading)
+                .frame(height:55).focused($isTyping)
+                .background(isTyping ? .blue : .gray, in: RoundedRectangle(cornerRadius: 14).stroke(lineWidth: 2))
+            Text(label)
+                .background(.ftitle.opacity(isTyping || !text.isEmpty ? 1 : 0))
+                .foregroundStyle(isTyping ? .blue : .gray)
+                .padding(.leading).offset(y:isTyping || !text.isEmpty ? -30 : 0)
+                .onTapGesture {
+                    isTyping.toggle()
+                }
+        }
+        .animation(.linear(duration:0.2),value: isTyping)
+                
+    }
+}
+
 #Preview {
-    TextFieldExample()
+    FormExample()
 }
